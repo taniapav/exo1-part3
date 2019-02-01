c98e9cda6e603f6241b5778909e390f2c269bb95
Tatjana <pavlovic.tatjana.p@gmail.com>
2019-02-01 08:52:59 +0100

    exo1 part3

---
+++ assets/js/js1.js
@@ -0,0 +1,8 @@
+//Je declare ma fonction.Elle permet d'afficher la bordure rouge autour de l'image.
+function changeBorder(){
+  document.getElementById('image1').style.border = '3px solid red';
+}
+//Je declare ma fonction.Elle permet de retirer la bordure rouge autour de l'image.
+function returnBorder(){
+  document.getElementById('image1').style.border = 'initial';
+}
